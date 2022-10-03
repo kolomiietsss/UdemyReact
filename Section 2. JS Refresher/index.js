826732f1a7418d3c4c5a532441b5ae52647e6c04
Artem Kolomiiets <akolomiets02@gmail.com>
2022-10-03 16:17:28 +0300

    Ended Section 2

--- Section 2. JS Refresher/index.js
+++ Section 2. JS Refresher/index.js
@@ -1,8 +1,25 @@
-
+// init
 let k = 1
 let i = 0
-
+//func + scope
 function pr() {
     console.log(`${k}, ${i}`);
 }
 pr(k)
+
+//spread
+let arr = [1,2,3];
+let newArr = [...arr, 4, 5];
+console.log(newArr);
+
+//rest rest operator and that merges the arguments into an array.
+function name(...args) {
+    console.log(args);
+}
+name(1,2,3); // makes array
+
+// Destructuring
+let [firstName, lastName] = ["1", "2"];
+
+console.log(firstName);
+console.log(lastName);
